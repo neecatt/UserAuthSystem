2654b8f2ba98b09b6e69b200e6b9a18cda9c16ad
HorrorOTB <nicatabdullazade135@gmail.com>
2023-08-26 02:15:14 +0400

    add two factor secret field

--- src/users/models/user.model.ts
+++ src/users/models/user.model.ts
@@ -2,12 +2,15 @@ import { Field, ID, ObjectType } from "@nestjs/graphql";
 import { Exclude } from "class-transformer";
 
 @ObjectType()
-export class User {
+export class UserType {
   @Field(() => ID)
   id: string;
 
   @Field(() => String)
   email: string;
+
+  @Field(() => String, { nullable: true })
+  twoFactorSecret: string;
 
   password: string;
 }
