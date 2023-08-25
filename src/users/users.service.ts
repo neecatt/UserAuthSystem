2d21cb7b678dfbd9581cf53f25adf41e24ad59b8
HorrorOTB <nicatabdullazade135@gmail.com>
2023-08-26 02:17:33 +0400

    2FA functionality methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { CreateUserInput } from "./dto/create-user.input";
 import { PrismaService } from "src/prisma.service";
+import { User } from "@prisma/client";
+import { authenticator } from "otplib";
 
 @Injectable()
 export class UsersService {
@@ -14,6 +16,37 @@ export class UsersService {
   async findOne(id: string) {
     return await this.prisma.user.findUnique({
       where: { id },
+    });
+  }
+
+  async setTwoFactorSecret(id: string, secret: string) {
+    console.log("id", id);
+    return await this.prisma.user.update({
+      where: { id },
+      data: { twoFactorSecret: secret },
+    });
+  }
+
+  async setTwoFactorStatus(userId: string, status: boolean) {
+    await this.prisma.user.update({
+      where: { id: userId },
+      data: { twoFactorEnabled: status },
+    });
+  }
+  async isTwoFactorAuthenticationCodeValid(
+    twoFactorAuthenticationCode: string,
+    user: User
+  ) {
+    const prisma_user = await this.prisma.user.findUnique({
+      where: { id: user.id },
+    });
+    if (!prisma_user || !prisma_user.twoFactorSecret) {
+      throw new BadRequestException("User or two-factor secret is missing");
+    }
+
+    return authenticator.verify({
+      token: twoFactorAuthenticationCode,
+      secret: prisma_user.twoFactorSecret,
     });
   }
 }
